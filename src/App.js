1d0eea3b827c24bbafaf1d7b15d2e5d00efaa649
Aaronokwok <aaronkwok101@gmail.com>
2022-04-22 22:19:26 -0700

    added stick nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"; 
 import Nav from "./Components/Nav";
+import Bio from "./Components/Bio"; 
 
 export default function App() {
     return (
         <div>
             <Nav />
+            <Bio />
         </div>
     )
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,9 +2,12 @@ import React from "react";
 
 export default function Nav() {
     return (
-        <header className="navBar">
-            <button>Aaron Codes</button>
-            <button >bar icon</button>
+        <header className="nav">
+            <p className="nav--ttl">AKodes</p>
+            <div className="nav--info">
+                <p>About</p>
+                <p>Projects</p> 
+            </div>  
         </header>
     )
 }
