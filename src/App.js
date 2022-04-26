9d7e478f60192220dc728480fb9ff88a187d76b1
Aaronokwok <aaronkwok101@gmail.com>
2022-04-25 18:20:31 -0700

    added Bio and Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"; 
 import Nav from "./Components/Nav";
 import Bio from "./Components/Bio"; 
+import Foot from "./Components/Foot"; 
 
 export default function App() {
     return (
         <div>
             <Nav />
             <Bio />
+            <Foot />
         </div>
     )
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,8 +3,9 @@ import React from "react";
 export default function Nav() {
     return (
         <header className="nav">
-            <p className="nav--ttl">AKodes</p>
+            <h1 className="nav--ttl">aKodes</h1>
             <div className="nav--info">
+                <p>Contact</p>
                 <p>About</p>
                 <p>Projects</p> 
             </div>  
