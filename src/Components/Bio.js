2994b92bff952a5e70778fd32c692d4192b0e6fd
Aaronokwok <aaronkwok101@gmail.com>
2023-01-16 21:26:45 -0800

    no changes, test commit

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -7,7 +7,7 @@ export default function Bio() {
             <hr />
             <p className="job">FRONTEND DEVELOPER BASED IN SANTA CLARA.</p>
             <p className="statement">EXPERIENCE BUILDING FRONTEND APPLICATIONS USING JAVASCRIPT, REACT, AND MORE.</p>
-            <a href="https://facebook.com" target="_blank" rel="noreferrer" className="resLink">Resume</a>
+            <a href="https://docs.google.com/document/d/e/2PACX-1vSARu0pcnMQjrXGzXSpmjIYFw8wMrCVNS-EZh4RZpGNEK9t1tc32-xcylaEFLkinTBOo3t-oyM1QgH9/pub" target="_blank" rel="noreferrer" className="resLink">Resume</a>
         </div>
     )
 }
