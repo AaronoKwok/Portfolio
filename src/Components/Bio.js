4ff7ee40cfc36cd32702c850481532aad1655205
Aaron Kwok <aaronkwok101@gmail.com>
2023-02-22 16:22:58 -0800

    add projects

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Bio() {
     return (
         <div>
-            <p className="job">FRONTEND DEVELOPER BASED IN THE SF BAY AREA.</p>
-            <p className="statement">EXPERIENCE BUILDING APPLICATIONS USING JAVASCRIPT, REACT, AND MORE.</p>
+            <p className="job">FRONTEND DEVELOPER BASED IN THE SF BAY AREA</p>
+            <p className="statement">EXPERIENCED IN BUILDING APPLICATIONS USING JAVASCRIPT, REACT, AND MORE</p>
         </div>
     )
 }
---
+++ src/Components/Foot.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+export default function Foot() {
+    return (
+        <footer className="foot">
+            
+        </footer>
+    )
+}
